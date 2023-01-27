dde74de9d736830add844f089530e843bd3369b5
sattarovmuhammad98@gmail.com <sattarovmuhammad98@gmail.com>
2023-01-27 23:49:49 +0500

    Initial Commit

---
+++ githubdemo/githubdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubdemoApp.swift
+//  githubdemo
+//
+//  Created by user on 27/01/23.
+//
+
+import SwiftUI
+
+@main
+struct githubdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
